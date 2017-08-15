5ecf2ca9adbf85085410a74c95781ca046d685ff
jy19921020 <691096060@qq.com>
2017-08-15 14:20:20 +0800

    远程部署测试1.02

--- docker/run.sh
+++ docker/run.sh
@@ -1,7 +1,7 @@
-#!/usr/bin/env bash //本次测试需要在异地进行，所以run的动作需要远程操作
+#!/usr/bin/env bash
 docker run -d \
 -p 8880:8880 \
 --name OCR_linkPage \
 -e spring.profiles.active="test" \
 -e server.port=8880 \
-jy19921020/testdocker:ocr
+localhost:5000/java-8
--- docker/stop.sh
+++ docker/stop.sh
@@ -1,2 +1,2 @@
-#!/usr/bin/env bash //由于本次生成的镜像不需要在本地跑，所以不需要暂停本地的容器
-docker stop jy19921020/testdocker:ocr && docker rm jy19921020/testdocker:ocr
+#!/usr/bin/env bash
+docker stop OCR_linkPage && docker rm OCR_linkPage
