c26a36b0e4d1b45bd1d911dc08bb93d52241d030
jy19921020 <691096060@qq.com>
2017-08-15 14:32:46 +0800

    远程部署测试1.03

--- docker/jenkins.sh
+++ docker/jenkins.sh
@@ -3,6 +3,7 @@ sh stop.sh
 sh build.sh
 
 #可以添加一个push动作，将该image push到dockerhub上
-docker push jy19921020/testdocker:ocr
+docker tag localhost:5000/java-8:latest docker.io/jy19921020/testdocker:ocr
+docker push docker.io/jy19921020/testdocker:ocr
 
 #sh run.sh
